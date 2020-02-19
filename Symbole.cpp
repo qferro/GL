7b07a2717e10365b3b4f7380c5910e314d237065
qferro <quentin.ferro@insa-lyon.fr>
2020-02-19 19:54:59 +0100

    Update Symbole.cpp

--- Symbole.cpp
+++ Symbole.cpp
@@ -3,29 +3,6 @@
 
 void Symbole::Affiche() {
    cout<<Etiquettes[ident];
-   	/*switch(ident){
-		case 0:
-			cout << "(";
-			break;
-		case 1:
-			cout << ")";
-			break;
-		case 2:
-			cout << "+";
-			break;
-		case 3: 
-			cout << "*";
-			break;
-		case 4:
-			cout << valeur;
-			break;
-		case 5:
-			cout << "$";
-			break;
-		case 6:
-			cout << "Erreur";
-			break;
-	}*/
 }
 
 void Entier::Affiche() {
